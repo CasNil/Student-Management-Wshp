253f280c04c32df094cd6f8f2e020ef4c0a10471
Casper Nilsson <caspernilsson13@gmail.com>
2024-09-11 10:28:21 +0200

    Finished part 1 of workshop.

--- src/main/java/se/lexicon/data_access/StudentDAO.java
+++ src/main/java/se/lexicon/data_access/StudentDAO.java
@@ -1,2 +1,13 @@
-package se.lexicon.data_access;public interface StudentDAO {
+package se.lexicon.data_access;
+import se.lexicon.models.Student;
+import java.util.List;
+
+public interface StudentDAO {
+    Student save(Student student);
+
+    Student find(int id);
+
+    List<Student> findAll();
+
+    void delete(int id);
 }
--- src/main/java/se/lexicon/data_access/StudentDAOListImpl.java
+++ src/main/java/se/lexicon/data_access/StudentDAOListImpl.java
@@ -1,2 +1,33 @@
-package se.lexicon.data_access;public class StudentDAOListImpl {
+package se.lexicon.data_access;
+
+import org.springframework.stereotype.Component;
+import se.lexicon.models.Student;
+
+import java.util.List;
+
+
+@Component
+public class StudentDAOListImpl implements StudentDAO {
+
+    List<Student> students;
+
+    @Override
+    public Student save(Student student) {
+        return null;
+    }
+
+    @Override
+    public Student find(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Student> findAll() {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
 }
